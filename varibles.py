4366272ee63d85b6223fe83dda2c150fd5e22bfd
Khush soni <sonikhush004@gmail.com>
2024-11-23 19:00:44 +0530

    Variables in python

---
+++ varibles.py
@@ -0,0 +1,34 @@
+#python is a case sensitve language-- (these are the way comments are in python)
+
+# name = "Khush"
+# age = 21
+# price = 243.23
+
+# a = 12
+# b = 5
+# sum = a + b 
+# print(sum) 
+# print(name)
+# print(type(name))
+# print(type(a))
+# print(type(price))
+# |keywords🗝 in python :
+# |------|-------|---------|--------|--------|
+# |and   |as     |break    |class   |continue|
+# |def   |del    |elif     |alse    |except  |
+# |for   |from   |False    |finally |global  |
+# |if    |in     |is       |import  |lambda  |
+# |not   |None   |nonlocal |or      |pass    |
+# |raise |return |True     |with    |while   |
+# |yield |-------|---------|--------|--------|
+
+
+name = (input("Enter your name = "))
+
+
+age = int(input("Enter your age: "))
+semester = int((input("Enter your semester Year:")))
+marks = float(input("enter your total marks:"))
+print("Hello",name,"Good Morning!")
+print("Your age is: ",age)
+print("Your Total marks: ",marks)
