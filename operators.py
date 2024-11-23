03a0cf820425df5579c772f226ae4f55858e5771
Khush soni <sonikhush004@gmail.com>
2024-11-23 19:00:26 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,41 @@
+#Arithmetic Operators:
+a = 12
+b = 12
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+
+# Relational operators
+c = 50
+d = 20
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+# print(a > b)
+# print(a < b)
+
+
+# Assignment Operators:
+num = 10
+num = num + 10
+# num += 10
+# num -= 10
+# num *= 10
+# num /= 10
+# num %= 10
+# print(num)
+a = 50
+b = 30
+print(not False)
+print(not (a > b ))
+
+val1 = False
+val2 = False
+
+print(val1 and val2)
+print(val1 or val2 )
